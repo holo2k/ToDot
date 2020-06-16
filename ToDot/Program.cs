992a9c61193b48132db5947719afb094533e37b1
holo2k <57012463+holo2k@users.noreply.github.com>
2020-06-16 15:25:23 +0500

    Add files via upload

---
+++ ToDot/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstFile = File.ReadAllText("1.txt", Encoding.GetEncoding("windows-1251"));
+            string secondFile = firstFile.Replace(',', '.');
+            File.WriteAllText("2.txt", secondFile);
+            Console.WriteLine("------------------------");
+            Console.WriteLine("Операция прошла успешно.");
+            Console.WriteLine("------------------------");
+            Console.ReadKey();
+        }
+    }
+}
